a6185b91127341aa70614aa1b7918b6f534ba8a1
Blajan David <david_blajan523@yahoo.com>
2024-04-22 21:52:36 +0300

    initial commit

--- src/main/java/dev/blaji/movies/security/CORSFilter.java
+++ src/main/java/dev/blaji/movies/security/CORSFilter.java
@@ -17,7 +17,7 @@ public class CORSFilter extends OncePerRequestFilter {
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "https://white-bay-0799f6703.5.azurestaticapps.net");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
